44937aab37d3d25903ee81b53eae0f7fe293480f
jiheek <blueskae_@naver.com>
2025-12-19 23:28:46 +0900

    feat: 설정 변경

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // publicPath: process.env.NODE_ENV === 'production'
-  publicPath: '/portfolio',
+  publicPath: '/portfolio/',
   outputDir: 'docs',
   css: {
     loaderOptions: {
